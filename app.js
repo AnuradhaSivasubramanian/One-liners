451fd9115ad1e7e217f20bbe69e63c1e05ab49a4
anu radha <anu.sivasubramanian@gmail.com>
2020-05-22 16:16:20 +0200

    attempt 1003

--- app.js
+++ app.js
@@ -45,7 +45,10 @@ app.use(function (err, req, res, next) {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use("/", express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+  });
 }
 
 module.exports = app;
