331305becdc77297def9eaee63a6f1f6c3acdd25
anu radha <anu.sivasubramanian@gmail.com>
2020-05-22 14:35:06 +0200

    attempt 4

--- database/config/db.config.js
+++ database/config/db.config.js
@@ -1,21 +1,9 @@
 require("dotenv").config();
+
 module.exports = {
-  development: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    name: process.env.DB_NAME,
-    dialect: "mysql",
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
-    use_env_variable: "JAWSDB_URL",
-    dialect: "mysql",
-  },
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  name: process.env.DB_NAME,
+  dialect: "mysql",
 };
--- database/models/index.js
+++ database/models/index.js
@@ -1,29 +1,11 @@
-let env = process.env.NODE_ENV || "development";
-let config = require(__dirname + "/../config/db.config.js")[env];
-
-require("dotenv").config();
+const envVars = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-if (config.use_env_variable) {
-  var connector = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  console.log("this is the connector");
-  var connector = new Sequelize("one_liners", "root", "5973", {
-    host: "localhost",
-    dialect: "mysql",
-  });
-}
-// if (process.env.NODE_ENV === "production") {
-//   connector = new Sequelize(envVars.name, envVars.user, envVars.password, {
-//     host: envVars.host,
-//     dialect: envVars.dialect,
-//   });
-// } else {
-//   connector = new Sequelize(envVars.name, envVars.user, envVars.password, {
-//     host: envVars.host,
-//     dialect: envVars.dialect,
-//   });
-// }
+
+const connector = new Sequelize(envVars.name, envVars.user, envVars.password, {
+  host: envVars.host,
+  dialect: envVars.dialect,
+});
 
 const authenticate = async (connector) => {
   try {
