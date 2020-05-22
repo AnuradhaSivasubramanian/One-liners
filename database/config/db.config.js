da282675a604407b8eded3cd02367803b1959009
anu radha <anu.sivasubramanian@gmail.com>
2020-05-22 15:58:17 +0200

    attempt 1001

--- database/config/db.config.js
+++ database/config/db.config.js
@@ -1,9 +1,30 @@
 require("dotenv").config();
 
 module.exports = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  name: process.env.DB_NAME,
-  dialect: "mysql",
+  development: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql",
+  },
 };
+
+// module.exports = {
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   name: process.env.DB_NAME,
+//   dialect: "mysql",
+// };
--- database/models/index.js
+++ database/models/index.js
@@ -1,11 +1,25 @@
-const envVars = require("../config/db.config.js");
+//const envVars = require("../config/db.config.js");
+
+var path = require("path");
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/db.config.js")[env];
 
 const Sequelize = require("sequelize");
 
-const connector = new Sequelize(envVars.name, envVars.user, envVars.password, {
-  host: envVars.host,
-  dialect: envVars.dialect,
-});
+// const connector = new Sequelize(envVars.name, envVars.user, envVars.password, {
+//   host: envVars.host,
+//   dialect: envVars.dialect,
+// });
+if (config.use_env_variable) {
+  var connector = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  console.log("here I am ");
+  var connector = new Sequelize(config.database, config.user, config.password, {
+    host: config.host,
+    dialect: config.dialect,
+  });
+  console.log(connector);
+}
 
 const authenticate = async (connector) => {
   try {
